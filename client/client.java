4468860a94192dcab09088a31d2b2d61ab71a40c
tdkim52 <tdkim52@gmail.com>
2015-04-09 20:02:42 -0700

    handles string buffer retrieval and printing

--- client/client.java
+++ client/client.java
@@ -19,25 +19,37 @@ import javax.swing.JOptionPane;
 public class Client {
 
    private BufferedReader in;
+   private static String DEFAULT = "140.160.137.170"; // linux-09.cs.wwu.edu
 
+   // attempts to connect to server and get coordinates
+   // reads buffer and prints coordinates
    public void connect(String addr) throws IOException {
       Socket socket = new Socket(addr, 49152);
       in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      String message = in.readLine();
-      System.out.println(message);
+      
+      String coordinate;
+      while ((coordinate = in.readLine()) != null) {
+         System.out.println(coordinate);
+      }
+      socket.close();
       System.exit(0);
    }
    
-   //
+   // gets server IP from user input and runs connect method
    public static void main(String[] args) throws IOException {
+   
+      Client client = new Client();
       
       Scanner reader = new Scanner(System.in);      
       System.out.println("Enter IP Address of server running service on port 49152:");
       String serverAddress = reader.nextLine();
       
-      Client client = new Client();
-      client.connect(serverAddress);
-     
+      if (serverAddress.equals("")) {
+         client.connect(DEFAULT);
+      }
+      else {
+         client.connect(serverAddress);
+      }
    }
 }
       
