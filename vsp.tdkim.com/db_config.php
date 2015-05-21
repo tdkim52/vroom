79565236187e76321ba3c59b2c81bdece002610a
Timothy Kim <donghyunkim52@gmail.com>
2015-05-21 14:37:44 -0700

    started  php webservice api for android db connection

---
+++ vsp.tdkim.com/db_config.php
@@ -0,0 +1,12 @@
+db_config.php
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "tdk_admin");
+define('DB_PWD', "admin");
+define('DB_DATABASE', "tdk_vsp");
+define('DB_SERVER', "localhost");
+?>
