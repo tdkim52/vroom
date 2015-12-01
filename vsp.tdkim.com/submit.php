e27856d56bed68770a4c43b40000cd0d5352f56c
Timothy Kim <donghyunkim52@gmail.com>
2015-12-01 14:28:20 -0800

    added expiration entry into query

--- vsp.tdkim.com/submit.php
+++ vsp.tdkim.com/submit.php
@@ -5,10 +5,10 @@ mysql_connect("localhost", "tdk_admin", "admin") or die(mysql_error());
 mysql_select_db("tdk_vsp") or die(mysql_error()); 
 
 if ($_POST['direction'] == 'X') {
-	mysql_query("INSERT INTO hazards (type, latitude, longitude, direction, message) VALUES ('$_POST[type_haz]', '$_POST[lat_text]', '$_POST[lng_text]', NULL, '$_POST[message]')");
+	mysql_query("INSERT INTO hazards (type, latitude, longitude, direction, message, expiration) VALUES ('$_POST[type_haz]', '$_POST[lat_text]', '$_POST[lng_text]', NULL, '$_POST[message]', '$_POST[time_text]')");
 }
 else {
-	mysql_query("INSERT INTO hazards (type, latitude, longitude, direction, message) VALUES ('$_POST[type_haz]', '$_POST[lat_text]', '$_POST[lng_text]', '$_POST[direction]', '$_POST[message]')");
+	mysql_query("INSERT INTO hazards (type, latitude, longitude, direction, message, expiration) VALUES ('$_POST[type_haz]', '$_POST[lat_text]', '$_POST[lng_text]', '$_POST[direction]', '$_POST[message]', '$_POST[time_text]')");
 
 }
 /*function add_to_database (){
